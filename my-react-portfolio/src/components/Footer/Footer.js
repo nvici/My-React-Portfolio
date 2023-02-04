f4ab8b2e0fae878a3493e1c0a0ae88c6a8d136c8
nvici <nvicinanza@outlook.com>
2023-02-04 14:23:47 -0600

    theme and profile photo added

---
+++ my-react-portfolio/src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import './Footer.css'
+
+const Footer = () => (
+  <footer className='footer'>
+    <a
+      href='https://github.com/nvici/My-React-Portfolio'
+      className='link footer__link'
+    >
+      Created By Nico Vicinanza
+    </a>
+  </footer>
+)
+
+export default Footer
---
+++ my-react-portfolio/src/portfolio.js
@@ -0,0 +1,62 @@
+const header = {
+  homepage: 'https://nvici.github.io/My-React-Portfolio',
+  title: 'Nico Vicinanza',
+}
+
+const about = {
+  name: 'Nico Vicinanza',
+  role: 'Front End Engineer',
+  description:
+    'Im a highly energized, hardworking, diligent, and a leader. Every job I do I give 100% because I love setting an example for others and I love helping others be successful. Years of being a customer focused individual has taught me the huge importance of each and every client.',
+  resume: 'https://docs.google.com/document/d/1O2Udfw-Lx3b_tYhBdHmpfUhN67soet7z/edit?usp=sharing&ouid=109315098127156025144&rtpof=true&sd=true',
+  social: {
+    linkedin: 'https://www.linkedin.com/in/nicolas-vicinanza-b49491161/',
+    github: 'https://github.com/nvici',
+  },
+}
+
+const projects = [
+  {
+    name: 'Playlist Generator',
+    description:
+      'A Playlist Generator, by searching a genre, randomly generates a new list of music with background information of that genre.',
+    stack: ['HTML', 'CSS', 'JavaScript'],
+    sourceCode: 'https://github.com/nvici/All-in-1-music',
+    livePreview: 'https://nvici.github.io/All-in-1-music/',
+  },
+  {
+    name: 'Know Mow Worries',
+    description:
+      'Know Mow Worries is a website where freelancers for yard work can post hiring ads and clientele can find freelancers in their local area to hire for yard work.',
+    stack: ['Node.js', 'Express', 'SQL'],
+    sourceCode: 'https://github.com/nvici/KnowMowWorries',
+    livePreview: 'https://blooming-eyrie-03886.herokuapp.com/',
+  },
+  {
+    name: 'Shoe Marketplace',
+    description:
+      'Jay-on-yo-feet is a shoe marketplace where you can buy shoes where prices are lower then anywhere else',
+    stack: ['React', 'NoSQL', 'Express'],
+    sourceCode: 'https://github.com/nvici/Jay-on-yo-feet',
+    livePreview: 'https://stormy-cliffs-65992.herokuapp.com/',
+  },
+]
+
+const skills = [
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'React',
+  'Node.js',
+  'Material UI',
+  'Express',
+  'SQL',
+  'NoSQL',
+  'MERN',
+]
+
+const contact = {
+  email: 'nvicinanza@outlook.com',
+}
+
+export { header, about, projects, skills,  contact }
